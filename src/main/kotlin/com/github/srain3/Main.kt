e51f97e8e19f8be3ba956abc22cb4f3813a1aec9
Kotlia <62182668+Kotlia@users.noreply.github.com>
2020-09-03 08:51:54 -0400

    Fix Class Main

--- src/main/kotlin/com/github/srain3/Main.kt
+++ src/main/kotlin/com/github/srain3/Main.kt
@@ -2,7 +2,7 @@ package com.github.srain3
 
 import org.bukkit.plugin.java.JavaPlugin
 
-class Main: JavaPlugin() {
+class Main : JavaPlugin() {
     override fun onEnable() {
         getCommand("oyasainews")?.setExecutor(OyasainewsCommand)
     }
