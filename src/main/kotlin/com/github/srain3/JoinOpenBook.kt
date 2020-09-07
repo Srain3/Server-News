c5f90ffa17fafa3210aebbc3905577cce01b11ec
Srain3 <srain3002@gmail.com>
2020-09-07 15:43:16 +0900

    v0.9.51
    I rewrote it from zero base and wrote a comment.
    Added the ability to add any number of books.
    Changed to display only when MainBook is updated at Join.
    Changed to pass in editable state instead of completed book when pulling out.

--- src/main/kotlin/com/github/srain3/JoinOpenBook.kt
+++ src/main/kotlin/com/github/srain3/JoinOpenBook.kt
@@ -3,15 +3,21 @@ package com.github.srain3
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
-import org.bukkit.inventory.ItemStack
 
-object JoinOpenBook : Listener{
-    private lateinit var book: ItemStack
-    fun getBook(main: Main){
-        book = main.i
+object JoinOpenBook: Listener {
+    private lateinit var main1: Main //この中でMainクラスを使うための(空の)変数設定
+    fun joinEv(main: Main){ //Mainから読み出されてMainクラスを参照する箱
+        main1 = main //main1にMainを入れる
+        return
     }
     @EventHandler
-    fun onJoinEvent(event: PlayerJoinEvent) {
-        event.player.openBook(book)
+    fun onJoinEvent(e: PlayerJoinEvent) { //プレイヤーjoin時に動く
+        //config内のプレイヤーの読んだmainbookのverと現在のmainbookのverが一致するか？データがない場合は0を返す
+        if(main1.config.getInt("playerdata.${e.player.name}", 0) != main1.mainver) {
+            //verが違う場合(読んでいない場合)
+            e.player.openBook(main1.mainbook) //メイン本を開く
+            main1.config.set("playerdata.${e.player.name}",main1.mainver) ; main1.saveConfig() // configへセーブ
+        }//verが同じ場合(読んでいる場合)
+        return
     }
 }
--- src/main/kotlin/com/github/srain3/Main.kt
+++ src/main/kotlin/com/github/srain3/Main.kt
@@ -1,8 +1,6 @@
 package com.github.srain3
 
-import net.luckperms.api.LuckPermsProvider
-import net.luckperms.api.context.ContextManager
-import net.luckperms.api.query.QueryOptions
+import org.bukkit.Material
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
@@ -10,66 +8,99 @@ import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
 
 class Main : JavaPlugin() {
-    lateinit var i: ItemStack
+    //変数作成
+    lateinit var mainbook: ItemStack
+    var mainver = 0
 
     override fun onEnable() {
-        this.saveDefaultConfig()
-        config
-        i = config.getItemStack("data")!!
-        server.pluginManager.registerEvents(JoinOpenBook, this)
-        JoinOpenBook.getBook(this)
+        saveDefaultConfig() //configがなければ作成
+        config //config読み込み
+        mainbook = config.getItemStack("main")!! //mainのItemStackデータを読み込み
+        mainver = config.getInt("mainversion") //mainversionのIntデータを読み込み
+        server.pluginManager.registerEvents(JoinOpenBook,this) //イベント登録(JoinEventで本を開く)
+        JoinOpenBook.joinEv(this) //JoinOpenBook内のfun joinEvにmainを渡す
     }
 
-    override fun onCommand(sender: CommandSender, cmd: Command, label: String, args: Array<out String>): Boolean {
-        if(sender is Player){
-            val luckperms = LuckPermsProvider.get()
-            fun hasPerm(p: Player, permission: String): Boolean {
-                if (!p.isOnline) throw IllegalArgumentException("Player is Offile")
-                val user = luckperms.userManager.getUser(p.uniqueId)!!
-                val contextManager: ContextManager = luckperms.contextManager
-                val contextSet = contextManager.getContext(user).orElseGet { contextManager.staticContext }
-                val permissionData = user.cachedData.getPermissionData(QueryOptions.contextual(contextSet))
-                return permissionData.checkPermission(permission).asBoolean()
-            }
-            if(args.isNotEmpty()) {
-                    if (args[0].equals("add")) {
-                        if(hasPerm(p = sender,permission = cmd.permission.toString()+".add")) {
-                            addItem(player = sender)
-                        } else {
-                            sender.sendMessage("オプションが違います")
-                            return false
-                        }
-                    } else {
-                        sender.sendMessage("オプションが違います")
-                        return false
-                    }
-                } else {
-                if (hasPerm(p = sender, permission = cmd.permission.toString())) {
-                    setItem(player = sender)
-                }
-            }
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        // oyasainewsコマンドを受け取ったら
+        if(sender is Player){ // コマンド送信元はPlayerか？
+            // コマンド送信元がPlayerの場合
+            if(args.isNullOrEmpty()){ // oyasainewsの後に引数は？
+                // 引数がない場合
+                sender.openBook(mainbook) // メインの本を開く
+                if(config.getInt("playerdata.${sender.name}") != mainver){ // ログイン時に開いたverと違うか？
+                    // 違う場合configへ保存
+                    config.set("playerdata.${sender.name}", mainver) ; saveConfig()
+                } //同じ場合スルー
                 return true
-
-        } else{
-            sender.sendMessage("プレイヤーのみ実行可能です")
-        }
+            } //引数がある場合
+            if(args[0].equals("add")){ // oyasainews addである場合
+                val item = sender.inventory.itemInMainHand // メインハンドのアイテム入手
+                if(item.type == Material.WRITTEN_BOOK){ //アイテムは記入済みの本？
+                    //記入済みの本だった場合
+                    if(args.getOrNull(1)!=null){ // oyasainews addの後に引数は？
+                        //引数がある場合
+                        config.set(args[1],item) ; saveConfig() // configへセーブ
+                        sender.sendMessage("${args[1]}を上書きしました")
+                        if(args[1].equals("main")){ // oyasainews add mainか？
+                            // mainである場合、関数mainbookを更新&mainversionカウントを増やす&configへ保存
+                            mainbook = item
+                            mainver += 1
+                            config.set("mainversion",mainver) ; saveConfig()
+                        } //mainじゃない場合スルー
+                        return true
+                    } //引数がない場合
+                    sender.sendMessage("/$label add [BookName]\nBookNameの部分が足りません！")
+                    return true
+                } //記入済みの本以外の場合
+                sender.sendMessage("記入済みの本を持って登録して下さい！")
+                return true
+            }
+            if(args[0].equals("open")){ // oyasainews openである場合
+                if(args.getOrNull(1)!=null){ // oyasainews openの後に引数は？
+                    //引数がある場合
+                    val book = config.getItemStack(args[1])
+                    if(book != null){ // [BookName]にデータはあるか？
+                        // データが有る場合
+                        sender.openBook(book)
+                        if(args[1].equals("main")){ // oyasainews open mainであるか？
+                            //mainだった場合、verの違いはあるか？
+                            if(config.getInt("playerdata.${sender.name}") != mainver){
+                                //verが違う場合、現在のverをconfigへ保存
+                                config.set("playerdata.${sender.name}", mainver) ; saveConfig()
+                            }//verが同じ場合スルー
+                        }
+                        return true
+                    } // データがない場合
+                    sender.sendMessage("${args[1]}は存在しません！")
+                    return true
+                } //引数がない場合
+                sender.sendMessage("/$label open [BookName]\nBookNameの部分が足りません！")
+                return true
+            }
+            if(args[0].equals("get")){ // oyasainews getである場合
+                if(args.getOrNull(1)!=null){ // oyasainews getの後に引数は？
+                    //引数がある場合
+                    val book = config.getItemStack(args[1])
+                    if(book != null){ // [BookName]にデータはあるか？
+                        val bookmeta = book.itemMeta
+                        val editbook = ItemStack(Material.WRITABLE_BOOK)
+                        editbook.itemMeta = bookmeta
+                        sender.inventory.setItemInMainHand(editbook)
+                        sender.sendMessage("${args[1]}を入手しました")
+                        return true
+                    } // データがない場合
+                    sender.sendMessage("${args[1]}は存在しません！")
+                    return true
+                } //引数がない場合
+                sender.sendMessage("/$label get [BookName]\nBookNameの部分が足りません！")
+                return true
+            } // 引数がヒットしない場合
+            sender.sendMessage("無効なオプションです")
+            return true
+        } // コマンド送信元がPlayerじゃない場合
+        sender.sendMessage("プレイヤーのみ実行可能です")
         return false
-    }
-
-    private fun addItem(player: Player) {
-        i = player.inventory.itemInMainHand
-        if(i.type.toString().equals("WRITTEN_BOOK")) {
-            config.set("data", i)
-            saveConfig()
-            player.sendMessage("お知らせを上書きしました")
-            JoinOpenBook.getBook(this)
-        } else {
-            i = config.getItemStack("data")!!
-            player.sendMessage("本じゃないアイテムです！上書きを中止しました")
-        }
-    }
-    private fun setItem(player: Player) {
-        player.openBook(i)
     }
 
     override fun onDisable() {
