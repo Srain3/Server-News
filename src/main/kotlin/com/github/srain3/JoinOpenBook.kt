16b6de4dd8810dc2a74dd5ea1aa6b8473dda2135
Srain3 <srain3002@gmail.com>
2020-09-08 19:01:05 +0900

    v1.0.1
    Bug fix when joining

--- src/main/kotlin/com/github/srain3/JoinOpenBook.kt
+++ src/main/kotlin/com/github/srain3/JoinOpenBook.kt
@@ -17,6 +17,10 @@ object JoinOpenBook: Listener {
             main1.config.set("spawnlocation",e.player.location) ; main1.saveConfig() //configにjoinしたプレイヤーのlocation登録して処理を止める
             return
         } //ある場合スルー
+        /*
+        動作テスト用
+         */
+        //e.player.sendMessage("あなたのWorld名は「${e.player.world.name}」で設定のSpawnWorld名は${main1.config.getLocation("spawnlocation")?.world?.name}です")
         if (e.player.world.name != main1.config.getLocation("spawnlocation")?.world?.name) { //worldはスポーンするワールドと一緒か？
             //一緒じゃない場合
             //config内のプレイヤーの読んだmainbookのverと現在のmainbookのverが一致するか？データがない場合は0を返す
@@ -28,9 +32,13 @@ object JoinOpenBook: Listener {
             return
         } //一緒の場合は座標で検知する(完全一致するとxzが0に)
         val px = e.player.location.x.toInt() ; val pz = e.player.location.z.toInt()
-        val spawnx = main1.config.getLocation("spawnlocation")!!.x.toInt()
-        val spawnz = main1.config.getLocation("spawnlocation")!!.z.toInt()
+        val spawnx = main1.config.getLocation("spawnlocation")?.x?.toInt() ?: return
+        val spawnz = main1.config.getLocation("spawnlocation")?.z?.toInt() ?: return
         val xz = px.minus(spawnx).plus(pz).minus(spawnz)
+        /*
+        動作テスト用
+         */
+        //e.player.sendMessage("あなたの座標はx${px},z${pz}でspawnの座標はx${spawnx},z${spawnz}で、結果は${xz}でした")
         if (xz == 0){ //完全一致(0)の場合何もしないで終える
             return
         }//一致しない(0以外)の場合
